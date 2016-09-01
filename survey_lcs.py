9e4b1a56be912b2d63d633d8655b4d307ebc4508
Brett Naul <brettnaul@gmail.com>
2016-08-31 19:25:50 -0700

    survey_lcs.py

---
+++ survey_lcs.py
@@ -0,0 +1,54 @@
+if __name__ == '__main__':
+    import glob
+    import numpy as np
+    import pandas as pd
+    from keras import backend as K
+    from keras.preprocessing.sequence import pad_sequences
+    import keras_util as ku
+    from autoencoder import uneven_gru_autoencoder
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("size", type=int)
+    parser.add_argument("num_layers", type=int)
+    parser.add_argument("drop_frac", type=float)
+    parser.add_argument("--batch_size", type=int, default=100)
+    parser.add_argument("--nb_epoch", type=int, default=100)
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--loss", type=str, default='mse')
+    parser.add_argument("--model_type", type=str, default='gru')
+    parser.add_argument("--gpu_frac", type=float, default=0.48)
+    parser.add_argument("--gpu_id", type=int, default=0)
+    parser.add_argument("--sim_type", type=str, default='survey_lcs')
+#    parser.add_argument("--filter", type=int, default=5)
+    parser.add_argument("--first_N", type=int, default=5000)
+    parser.add_argument("--n_min", type=int, default=100)
+    args = parser.parse_args()
+
+    np.random.seed(0)
+
+    filenames = glob.glob('./data/survey_lcs/*')
+    lengths = {f: sum(1 for line in open(f)) for f in filenames}
+    filenames = [f for f in filenames if lengths[f] >= args.n_min]
+    filenames = sorted(filenames, key=lambda f: lengths[f])
+
+    X_list = [np.loadtxt(f, delimiter=',') for f in filenames[:args.first_N]]
+
+    model_dict = {'gru': uneven_gru_autoencoder}
+    K.set_session(ku.limited_memory_session(args.gpu_frac, args.gpu_id))
+    X = pad_sequences(X_list, value=-1., dtype='float')
+    model = model_dict[args.model_type](input_len=X.shape[-1], aux_input_len=2,
+                                        n_step=X.shape[1], size=args.size,
+                                        num_layers=args.num_layers,
+                                        drop_frac=args.drop_frac)
+
+    run = "{}_{:03d}_x{}_{:1.0e}_drop{}".format(args.model_type, args.size,
+                                                args.num_layers, args.lr,
+                                                int(100 * args.drop_frac)).replace('e-', 'm')
+    if 'conv' in run:
+        run += '_f{}'.format(args.filter)
+
+    history = ku.train_and_log({'main_input': X, 'aux_input': X[:, :, [0, 2]]}, X[:, :, 1:2],
+                               run, model, lr=args.lr, nb_epoch=args.nb_epoch,
+                               batch_size=args.batch_size, loss=args.loss,
+                               sim_type=args.sim_type)
