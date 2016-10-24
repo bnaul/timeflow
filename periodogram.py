ef744cd791505c4abdd7e21a3e306f49524e6908
Brett Naul <brettnaul@gmail.com>
2016-10-24 10:42:29 -0400

    Periodogram estimator

--- periodogram.py
+++ periodogram.py
@@ -1,16 +1,17 @@
 import numpy as np
 from keras import backend as K
 from keras.layers import (Input, Dense, TimeDistributed, Activation, LSTM, GRU,
-                          Dropout, merge, Reshape, Flatten, RepeatVector)
+                          Dropout, merge, Reshape, Flatten, RepeatVector,
+                          Conv1D, AtrousConv1D, MaxPooling1D)
 from keras.models import Model, Sequential
 
 import sample_data
 import keras_util as ku
 
 
-def even_lstm_period_estimator(output_len, n_step, size, num_layers, drop_frac, **kwargs):
+def lstm_periodogram_estimator(output_len, input_size, n_step, size, num_layers, drop_frac, **kwargs):
     model = Sequential()
-    model.add(LSTM(size, input_shape=(n_max, 1),
+    model.add(LSTM(size, input_shape=(n_step, input_size),
                    return_sequences=(num_layers > 1)))
     for i in range(1, num_layers):
         model.add(Dropout(drop_frac))
@@ -19,9 +20,9 @@ def even_lstm_period_estimator(output_len, n_step, size, num_layers, drop_frac, 
     return model
 
 
-def even_gru_period_estimator(output_len, n_step, size, num_layers, drop_frac, **kwargs):
+def gru_periodogram_estimator(output_len, input_size, n_step, size, num_layers, drop_frac, **kwargs):
     model = Sequential()
-    model.add(GRU(size, input_shape=(n_max, 1),
+    model.add(GRU(size, input_shape=(n_step, input_size),
                    return_sequences=(num_layers > 1)))
     for i in range(1, num_layers):
         model.add(Dropout(drop_frac))
@@ -30,33 +31,55 @@ def even_gru_period_estimator(output_len, n_step, size, num_layers, drop_frac, *
     return model
 
 
+def conv_periodogram_estimator(output_len, input_size, n_step, size, num_layers, drop_frac, **kwargs):
+    model = Sequential()
+    model.add(Conv1D(size, kwargs['filter'], activation='relu',
+                     input_shape=(n_step, input_size)))
+#    model.add(MaxPooling1D(5))
+    model.add(Dropout(drop_frac))
+    for i in range(1, num_layers):
+        model.add(Conv1D(size, kwargs['filter'], activation='relu',
+                         input_shape=(n_step, input_size)))
+#        model.add(MaxPooling1D(5))
+        model.add(Dropout(drop_frac))
+    model.add(Flatten())
+    model.add(Dense(size, activation='relu'))
+    model.add(Dense(output_len, activation='linear'))
+    return model
+
+
+def atrous_periodogram_estimator(output_len, input_size, n_step, size, num_layers, drop_frac, **kwargs):
+    model = Sequential()
+    # TODO try tanh * sigmoid activation, other intializations?
+    model.add(AtrousConv1D(size, kwargs['filter'], activation='relu',
+                           input_shape=(n_step, input_size),
+                           border_mode='valid',
+#                           causal=True,
+                           atrous_rate=1))
+    model.add(Dropout(drop_frac))
+    for i in range(1, num_layers):
+        model.add(AtrousConv1D(size, kwargs['filter'], activation='relu',
+                               input_shape=(n_step, input_size),
+                               border_mode='valid',
+#                               causal=True,
+                               atrous_rate=2 ** i))
+        model.add(Dropout(drop_frac))
+    model.add(Flatten())
+    model.add(Dense(size, activation='relu'))
+    model.add(Dense(output_len, activation='linear'))
+    return model
+
+
 if __name__ == '__main__':
     import argparse
     import os
     import numpy as np
-    from scipy.fftpack import fft, dct
     from keras import backend as K
+    from scipy.fftpack import fft
+    from gatspy.periodic import LombScargleFast
 
     import sample_data
     import keras_util as ku
-
-    np.random.seed(0)
-    SIM_TYPE = os.path.splitext(os.path.basename(__file__))[0]
-    N_train = 50000; N_test = 1000
-    N = N_train + N_test
-    train = np.arange(N_train); test = np.arange(N_test) + N_train
-    n_min = 250; n_max = 250
-    X_raw, Y = sample_data.periodic(N, n_min, n_max, t_max=2*np.pi, even=True,
-                                A_shape=5., noise_sigma=2e-9, w_min=0.1,
-                                w_max=1.)
-    X_raw = X_raw[:, :, 1:2]
-    Z = np.abs(fft(X_raw))
-
-    sigma = 2e-0
-    X = X_raw + np.random.normal(size=X_raw.shape, scale=sigma)
-
-    model_dict = {'lstm': even_lstm_period_estimator, 'gru': even_gru_period_estimator}
-#                  'relu': even_relu_period_estimator, 'sin': even_sin_period_estimator}
 
     import argparse
     parser = argparse.ArgumentParser()
@@ -67,17 +90,51 @@ if __name__ == '__main__':
     parser.add_argument("--nb_epoch", type=int, default=250)
     parser.add_argument("--lr", type=float, default=0.002)
     parser.add_argument("--loss", type=str, default='mse')
+    parser.add_argument("--loss_weights", type=float, nargs='*')
     parser.add_argument("--model_type", type=str, default='lstm')
     parser.add_argument("--gpu_frac", type=float, default=0.31)
     parser.add_argument("--gpu_id", type=int, default=0)
+    parser.add_argument("--sigma", type=float, default=2e-9)
+    parser.add_argument("--sim_type", type=str, default='periodogram')
+    parser.add_argument("--filter", type=int, default=5)
+    parser.add_argument("--N_train", type=int, default=50000)
+    parser.add_argument("--N_test", type=int, default=1000)
+    parser.add_argument("--n_min", type=int, default=250)
+    parser.add_argument("--n_max", type=int, default=250)
+    parser.add_argument('--even', dest='even', action='store_true')
+    parser.add_argument('--uneven', dest='even', action='store_false')
+    parser.add_argument('--no_train', dest='no_train', action='store_true')
+    parser.set_defaults(even=True)
     args = parser.parse_args()
 
+    np.random.seed(0)
+    N = args.N_train + args.N_test
+    train = np.arange(args.N_train); test = np.arange(args.N_test) + args.N_train
+    X, Y = sample_data.periodic(N, args.n_min, args.n_max, t_max=2*np.pi, even=args.even,
+                                A_shape=5., noise_sigma=args.sigma, w_min=0.1,
+                                w_max=1.)
+    Y[:, 0] **= -1  # period instead of frequency
+    
+    if args.loss_weights:
+        Y *= args.loss_weights
+
+    if args.even:
+        X = X[:, :, 1:2]
+        F = dct(X)[:, :, 0]
+    else:
+        raise NotImplementedError("Use Lomb-Scargle here")
+
+    model_dict = {'lstm': lstm_periodogram_estimator, 'gru': gru_periodogram_estimator,
+                  'conv': conv_periodogram_estimator, 'atrous': atrous_periodogram_estimator}
+
     K.set_session(ku.limited_memory_session(args.gpu_frac, args.gpu_id))
-    model = model_dict[args.model_type](output_len=Y.shape[-1], n_step=n_max,
-                                        **vars(args))
+    model = model_dict[args.model_type](output_len=F.shape[-1],
+                                        input_size=X.shape[-1],
+                                        n_step=args.n_max, **vars(args))
 
     run = "{}_{:03d}_x{}_{:1.0e}_drop{}".format(args.model_type, args.size,
                                                 args.num_layers, args.lr,
                                                 int(100 * args.drop_frac)).replace('e-', 'm')
-    history = ku.train_and_log(X[train], Y[train], run, model,
-                               sim_type=SIM_TYPE, **vars(args))
+    if 'conv' in run:
+        run += '_f{}'.format(args.filter)
+    history = ku.train_and_log(X[train], F[train], run, model, **vars(args))
