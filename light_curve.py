d6fbaa77c856f8dba3fe4a090acb5826ee6043f7
Brett Naul <brettnaul@gmail.com>
2017-03-06 12:13:00 -0800

    Add LINEAR autoencoder

--- light_curve.py
+++ light_curve.py
@@ -38,7 +38,16 @@ class LightCurve():
                            best_score=self.best_score, label=self.label)
                 for s in splits]
 
-    def load_all():
+    def fit_lomb_scargle(self):
+        period_range = (0.005 * (max(self.times) - min(self.times)),
+                        0.95 * (max(self.times) - min(self.times)))
+        model_gat = LombScargleFast(fit_period=True, silence_warnings=True,
+            optimizer_kwds={'period_range': period_range, 'quiet': True})
+        model_gat.fit(self.times, self.measurements, self.errors)
+        self.best_period = model_gat.best_period
+        self.best_score = model_gat.score(model_gat.best_period).item()
+
+    def load_asas():
         light_curves = []
         bigmacc = pd.read_csv('data/asas/bigmacc.txt', delimiter='\t', index_col='ASAS_ID')
         for fname in glob.glob('./data/asas/*/*'):
@@ -52,18 +61,37 @@ class LightCurve():
                                     times=df.HJD.values, measurements=df.MAG_0.values,
                                     errors=df.MER_0.values)
                     lc.label = bigmacc.loc[lc.name].CLASS if lc.name in bigmacc.index else None
-                    period_range = (0.005 * (max(lc.times) - min(lc.times)),
-                                    0.95 * (max(lc.times) - min(lc.times)))
-                    model_gat = LombScargleFast(fit_period=True, silence_warnings=True,
-                        optimizer_kwds={'period_range': period_range, 'quiet': True})
-                    model_gat.fit(lc.times, lc.measurements, lc.errors)
-                    lc.best_period = model_gat.best_period
-                    lc.best_score = model_gat.score(model_gat.best_period).item()
+                    lc.fit_lomb_scargle()
                     light_curves.append(lc)
+        return light_curves
+
+    def load_linear():
+        header_fname = 'data/linear/LINEARattributesFinalApr2013.dat'
+        light_curves = []
+        header = pd.read_table(header_fname, comment='#', header=None,
+                               delim_whitespace=True)
+        colnames = [l for l in open(header_fname) if
+                    l[0] == '#'][-1].lstrip('#').split()
+        header.columns = colnames
+        header.set_index('LINEARobjectID', inplace=True)
+        LC_types = ['RR_Lyrae_ab', 'RR_Lyrae_c', '???', 'Algol',
+                    'Contact_Binary', 'Delta_Scuti']
+
+        for fname in glob.glob('./data/linear/lc/*'):
+            df = pd.read_csv(fname, header=0)
+            df.drop_duplicates(subset=['mjd'], keep='first', inplace=True)
+            lc = LightCurve(name=os.path.splitext(os.path.basename(fname))[0],
+                            survey='LINEAR', times=df.mjd.values,
+                            measurements=df.m.values, errors=df.merr.values)
+            lc.label = LC_types[header.LCtype.loc[int(lc.name)] - 1]
+            lc.fit_lomb_scargle()
+            light_curves.append(lc)
         return light_curves
 
 
 if __name__ == "__main__":
     print("Adding light curve data")
-    light_curves = LightCurve.load_all()
-    joblib.dump(light_curves, 'light_curves.pkl', compress=3)
+#    light_curves = LightCurve.load_asas()
+#    joblib.dump(light_curves, 'asas.pkl', compress=3)
+    light_curves = LightCurve.load_linear()
+    joblib.dump(light_curves, 'linear.pkl', compress=3)
